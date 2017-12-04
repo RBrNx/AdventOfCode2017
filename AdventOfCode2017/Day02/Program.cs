5cd5db637d992a62be9dac46fa5f4589958f8bfb
Conor Watson <conor.watson94@gmail.com>
2017-12-04 11:55:34 +0000

    Day02 Part 2 Complete!

--- AdventOfCode2017/Day02/Program.cs
+++ AdventOfCode2017/Day02/Program.cs
@@ -8,13 +8,28 @@ namespace Day02
 {
     class Program
     {
-        static void Main(string[] args)
+        static string[] lines = File.ReadAllLines("input.txt");
+
+        static int findDivisor(List<int> numbers)
         {
-            string[] lines = File.ReadAllLines("input.txt");
-            Regex inputRegex = new Regex(@"(\d+)");
+            int divisorResult = 0;
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                for(int j = 0; j < numbers.Count(); j++)
+                {
+                    if (numbers[i] == numbers[j]) continue;
+                    if (numbers[i] % numbers[j] == 0) divisorResult = numbers[i] / numbers[j];
+                }
+            }
+
+            return divisorResult;
+        }
+
+        static void PartOne() {           
             int checksum = 0;
 
-            for(int i = 0; i < lines.Length; i++)
+            for (int i = 0; i < lines.Length; i++)
             {
                 MatchCollection matchList = Regex.Matches(lines[i], @"(\d+)");
                 List<int> row = matchList.Cast<Match>().Select(Match => int.Parse(Match.Value)).ToList();
@@ -23,6 +38,26 @@ namespace Day02
             }
 
             Console.WriteLine("Checksum is: " + checksum);
+        }
+
+        static void PartTwo() {
+            int resultsSum = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                MatchCollection matchList = Regex.Matches(lines[i], @"(\d+)");
+                List<int> row = matchList.Cast<Match>().Select(Match => int.Parse(Match.Value)).ToList();
+
+                resultsSum += findDivisor(row);
+            }
+
+            Console.WriteLine("Sum of the Results is: " + resultsSum);
+        }
+
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
             Console.Read();
         }
     }
