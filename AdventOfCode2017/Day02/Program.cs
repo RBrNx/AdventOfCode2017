7ae573918ea17a777101a9321a4201126c55ba4d
Conor Watson <conor.watson94@gmail.com>
2017-12-04 11:34:55 +0000

    Day02 Part 1 Complete

--- AdventOfCode2017/Day02/Program.cs
+++ AdventOfCode2017/Day02/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day02
 {
@@ -6,7 +10,20 @@ namespace Day02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = File.ReadAllLines("input.txt");
+            Regex inputRegex = new Regex(@"(\d+)");
+            int checksum = 0;
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                MatchCollection matchList = Regex.Matches(lines[i], @"(\d+)");
+                List<int> row = matchList.Cast<Match>().Select(Match => int.Parse(Match.Value)).ToList();
+
+                checksum += (row.Max() - row.Min());
+            }
+
+            Console.WriteLine("Checksum is: " + checksum);
+            Console.Read();
         }
     }
 }
