1f256ecc7e96107e0dd18e31b16ba2e1cb9d9405
RBrNx <conor.watson94@gmail.com>
2017-12-05 10:37:08 +0000

    Day04 Part 1 and 2 complete!

--- AdventOfCode2017/Day04/Program.cs
+++ AdventOfCode2017/Day04/Program.cs
@@ -3,13 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Day04
 {
     class Program
     {
+        static string[] lines = File.ReadAllLines("../../input.txt");
+
+        static void PartOne() {
+            int validCount = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                MatchCollection matchList = Regex.Matches(lines[i], @"(\w+)");
+                List<string> row = matchList.Cast<Match>().Select(Match => Match.Value).ToList();
+
+                if (!row.GroupBy(x => x).Any(g => g.Count() > 1))
+                {
+                    validCount++;
+                }
+            }
+
+            Console.WriteLine("There are " + validCount + " valid Passphrases");
+        }
+
+        static void PartTwo() {
+            int validCount = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                MatchCollection matchList = Regex.Matches(lines[i], @"(\w+)");
+                List<string> row = matchList.Cast<Match>().Select(Match => Match.Value).ToList();
+
+                bool valid = true;
+                for(int a = 0; a < row.Count; a++)
+                {
+                    for (int b = 0; b < row.Count; b++)
+                    {
+                        if(a != b)
+                        {
+                            if(row[a].OrderBy(c => c).SequenceEqual(row[b].OrderBy(c => c)))
+                            {
+                                valid = false;
+                            }
+                        }
+                    }
+                }
+
+                if (valid) validCount++;
+            }
+
+            Console.WriteLine("There are " + validCount + " valid Passphrases");
+            Console.Read();
+        }
+
         static void Main(string[] args)
         {
+            PartOne();
+            PartTwo();
         }
     }
 }
