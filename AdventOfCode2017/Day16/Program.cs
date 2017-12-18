4fd3d11bf5e64485d053b0b9cf573c205b4867ef
RBrNx <conor.watson94@gmail.com>
2017-12-18 15:25:42 +0000

    Day 16 Part 1 and 2 Complete! Day 17 Part 1 Complete!

---
+++ AdventOfCode2017/Day16/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Numerics;
+
+namespace Day16
+{
+    class Program
+    {
+        private static void spin(ref string[] programs, int count)
+        {
+            string[] newArr = (string[])programs.Clone();
+            for(int loop = 0; loop < count; loop++)
+            {
+                for (int i = 0; i < programs.Length; i++)
+                {
+                    newArr[(i + 1) % newArr.Length] = programs[i];
+                }
+                programs = (string[])newArr.Clone();
+            }
+        }
+
+        static void exchange(ref string[] programs, int posA, int posB)
+        {
+            string A = programs[posA];
+            programs[posA] = programs[posB];
+            programs[posB] = A;
+        }
+
+        static void partner(ref string[] programs, string nameA, string nameB)
+        {
+            int posA = Array.IndexOf(programs, nameA);
+            int posB = Array.IndexOf(programs, nameB);
+            exchange(ref programs, posA, posB);
+        }
+
+        static void printPrograms(string[] programs)
+        {
+            for (int i = 0; i < programs.Count(); i++)
+            {
+                Console.Write(programs[i]);
+            }
+            Console.WriteLine("");
+        }
+
+        static void performDance(ref string[] programs, List<string> danceMoves)
+        {
+            for (int i = 0; i < danceMoves.Count; i++)
+            {
+                string dMove = danceMoves[i];
+
+                if (dMove[0] == 's')
+                {
+                    int spinCount = int.Parse(dMove.Substring(1));
+                    spin(ref programs, spinCount);
+                }
+                else if (dMove[0] == 'x')
+                {
+                    int[] programsPos = dMove.Substring(1).Split('/').Select(x => int.Parse(x)).ToArray();
+                    exchange(ref programs, programsPos[0], programsPos[1]);
+                }
+                else if (dMove[0] == 'p')
+                {
+                    string[] programNames = dMove.Substring(1).Split('/');
+                    partner(ref programs, programNames[0], programNames[1]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] programs = Enumerable.Range('a', 16).Select(x => ((char)x).ToString()).ToArray();
+            string[] programsCopy = (string[])programs.Clone();
+            List<string> danceMoves = File.ReadAllText("../../input.txt").Split(',').ToList();
+            Dictionary<int, int> exchanges = new Dictionary<int, int>();
+            int backToStart = 1;
+
+            performDance(ref programsCopy, danceMoves);
+            printPrograms(programsCopy);
+
+            while (!Enumerable.SequenceEqual(programsCopy, programs))
+            {
+                backToStart++;
+                performDance(ref programsCopy, danceMoves);               
+            }
+
+            for(int i = 0; i < (1000000000 % backToStart); i++)
+            {
+                performDance(ref programsCopy, danceMoves);
+            }
+            
+            printPrograms(programsCopy);
+            Console.Read();
+        }
+    }
+}
---
+++ AdventOfCode2017/Day17/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+    class bufferElement
+    {
+        public int position;
+        public int value;
+
+        public bufferElement(int p, int v)
+        {
+            position = p;
+            value = v;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int currentPos = 0;
+            int position2018 = 0;
+            int position0 = 0;
+            int input = 369;
+            List<bufferElement> buffer = new List<bufferElement>() { new bufferElement(0, 0) };
+
+            for (int i = 1; i <= 50000000; i++)
+            {
+                int pos = ((currentPos + input) % buffer.Count) + 1;
+                buffer.Where(e => e.position >= pos).ToList().ForEach(e => e.position++);
+                buffer.Add(new bufferElement(pos, i));
+                currentPos = pos;
+
+                if (i == 2017)
+                {
+                    int p = buffer.Find(x => x.value == 2017).position;
+                    Console.WriteLine("The value after 2017 is: " + buffer.Find(x => x.position == p + 1).value);
+                }
+                if (i == 50000000)
+                {
+                    int p = buffer.FindIndex(x => x.value == 0);
+                    Console.WriteLine("The value after 2017 is: " + buffer.Find(x => x.position == p + 1).value);
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
