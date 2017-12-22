bd73d6d7525910cabf712b4fd6975f8f4775a89c
RBrNx <conor.watson94@gmail.com>
2017-12-22 11:35:38 +0000

    Day 20 and 21 Parts 1 and 2 Complete!

---
+++ AdventOfCode2017/Day20/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Day20
+{
+    class Particle
+    {
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public Vector3 Acceleration;
+        public int id;
+
+        public Particle(Vector3 pos, Vector3 vel, Vector3 acc, int _id)
+        {
+            Position = pos;
+            Velocity = vel;
+            Acceleration = acc;
+            id = _id;
+        }
+
+        public Particle(Particle p)
+        {
+            Position = new Vector3(p.Position.x, p.Position.y, p.Position.z);
+            Velocity = new Vector3(p.Velocity.x, p.Velocity.y, p.Velocity.z);
+            Acceleration = new Vector3(p.Acceleration.x, p.Acceleration.y, p.Acceleration.z);
+            id = p.id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Particle p = (Particle)obj;
+            return (id == p.id);
+        }
+
+        public override int GetHashCode()
+        {
+            return id;
+        }
+
+        public void IncreaseVelocity(Vector3 acc)
+        {
+            Velocity.x += acc.x;
+            Velocity.y += acc.y;
+            Velocity.z += acc.z;
+        }
+
+        public void IncreasePosition(Vector3 vel)
+        {
+            Position.x += vel.x;
+            Position.y += vel.y;
+            Position.z += vel.z;
+        }
+
+        public void Move()
+        {
+            IncreaseVelocity(Acceleration);
+            IncreasePosition(Velocity);
+        }
+
+        public bool isEqualPosition(Particle p)
+        {
+            return (Position.x == p.Position.x && Position.y == p.Position.y && Position.z == p.Position.z);
+        }
+
+        public int DistanceFrom(Vector3 point)
+        {
+            return Math.Abs(Position.x - point.x) + Math.Abs(Position.y - point.y) + Math.Abs(Position.z - point.z);
+        }
+    }
+
+    class Vector3
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Vector3(int _x, int _y, int _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+        public Vector3(int[] arr)
+        {
+            x = arr[0];
+            y = arr[1];
+            z = arr[2];
+        }
+    }
+
+    class Program
+    {
+
+        static void PartOne(List<Particle> particles) {
+            Dictionary<Particle, int> closestParticle = new Dictionary<Particle, int>();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                int smallestDist = int.MaxValue;
+                Particle closest = null;
+
+                foreach (Particle p in particles)
+                {
+                    int dist = p.DistanceFrom(new Vector3(0, 0, 0));
+                    if (dist < smallestDist)
+                    {
+                        smallestDist = dist;
+                        closest = p;
+                    }
+                    p.Move();
+                }
+
+                if (closestParticle.ContainsKey(closest))
+                {
+                    closestParticle[closest]++;
+                }
+                else
+                {
+                    closestParticle.Add(closest, 1);
+                }
+            }
+
+            Console.WriteLine("The particle that will stay closest to <0, 0, 0> is particle: " + closestParticle.FirstOrDefault(x => x.Value == closestParticle.Values.Max()).Key.id);
+        }
+
+        static void PartTwo(List<Particle> particles) {
+            for (int i = 0; i < 5000; i++)
+            {
+                List<Particle> collided = new List<Particle>();
+
+                foreach (Particle particle in particles)
+                {
+                    foreach (Particle p in particles)
+                    {
+                        if (!particle.Equals(p) && particle.isEqualPosition(p))
+                        {
+                            if(!collided.Contains(particle)) collided.Add(particle);
+                        }
+                    }
+                }
+
+                particles = particles.Except(collided).ToList();
+
+                foreach (Particle p in particles)
+                {
+                    p.Move();                    
+                } 
+            }
+
+            Console.WriteLine("There are " + particles.Count() + " particles left");
+        }
+
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../input.txt");
+            List<Particle> particles = new List<Particle>();
+            List<Particle> collided = new List<Particle>();
+            
+
+            for(int i = 0; i < lines.Length; i++)
+            {
+                MatchCollection matches = Regex.Matches(lines[i], @"(?:\w+)=<(-?\d+,-?\d+,-?\d+)>");
+                Vector3 pos = new Vector3(matches[0].Groups[1].Value.Split(',').Select(x => int.Parse(x)).ToArray());
+                Vector3 vel = new Vector3(matches[1].Groups[1].Value.Split(',').Select(x => int.Parse(x)).ToArray());
+                Vector3 acc = new Vector3(matches[2].Groups[1].Value.Split(',').Select(x => int.Parse(x)).ToArray());
+                particles.Add(new Particle(pos, vel, acc, i));
+            }
+
+            PartOne(particles.Select(x => new Particle(x)).ToList());
+            PartTwo(particles.Select(x => new Particle(x)).ToList());
+
+            Console.Read();
+        }
+    }
+}
---
+++ AdventOfCode2017/Day21/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Day21
+{
+    public class GridComparer : IEqualityComparer<string[][]>
+    {
+        public bool Equals(string[][] a, string[][] b)
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (!a[i].SequenceEqual(b[i])) return false;
+            }
+
+            return true;
+        }
+
+        public int GetHashCode(string[][] grid)
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    class Program
+    {
+        static string[][] Convert1Dto2D(string pattern)
+        {
+            string[] lines = pattern.Split('/');
+            string[][] Pattern2D = new string[lines.Length][].Select(x => new string[lines.Length]).ToArray();
+
+            for(int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    Pattern2D[y][x] = lines[y][x].ToString();
+                }
+            }
+
+            return Pattern2D;
+        }
+
+        static List<string[][]> splitGrid(string[][] grid)
+        {
+            List<string[][]> split = new List<string[][]>();
+            int divisor = (grid.GetLength(0) % 2 == 0) ? 2 : 3;
+            int splitLength = grid.GetLength(0) / divisor;
+
+            for(int yDim = 0; yDim < grid.GetLength(0); yDim += divisor)
+            {
+                for (int xDim = 0; xDim < grid.GetLength(0); xDim += divisor)
+                {
+                    string[][] newGrid = new string[divisor][].Select(x => new string[divisor]).ToArray();
+
+                    for (int y = 0; y < divisor; y++)
+                    {
+                        for (int x = 0; x < divisor; x++)
+                        {
+                            newGrid[y][x] = grid[yDim + y][xDim + x];
+                        }
+                    }
+
+                    split.Add(newGrid);
+                }
+            }
+            
+ 
+            return split;
+        }
+
+        static string[][] findMatch(string[][] grid, Dictionary<string[][], string[][]> patternRules)
+        {
+            string[][] rotation = grid.Select(a => a.ToArray()).ToArray();
+            Dictionary<string[][], int> test = new Dictionary<string[][], int>(new GridComparer());
+
+            for (int rotations = 0; rotations < 4; rotations++)
+            {
+                rotation = rotateGrid(rotation);
+                string[][] flippedH = flipGridHorz(rotation.Select(a => a.ToArray()).ToArray());
+
+                if(!test.ContainsKey(rotation)) test.Add(rotation, 0);
+                if (!test.ContainsKey(flippedH)) test.Add(flippedH, 0);
+
+                if (patternRules.ContainsKey(rotation))
+                {
+                    return patternRules[rotation];
+                }
+                else if (patternRules.ContainsKey(flippedH))
+                {
+                    return patternRules[flippedH];
+                }
+            }
+
+            return null;
+        }
+
+        static string[][] rotateGrid(string[][] grid)
+        {
+            grid = transpose(grid);
+            for(int i = 0; i < grid.Length; i++)
+            {
+                grid[i] = grid[i].Reverse().ToArray();
+            }
+
+            return grid;
+        }
+
+        static string[][] flipGridHorz(string[][] grid)
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                grid[i] = grid[i].Reverse().ToArray();
+            }
+
+            return grid;
+        }
+
+        static string[][] flipGridVert(string[][] grid)
+        {
+            for (int y = 0; y < grid.Length; y++)
+            {
+                string temp = grid[0][y];
+                grid[0][y] = grid[grid.Length - 1][y];
+                grid[grid.Length - 1][y] = temp;
+            }
+
+            return grid;
+        }
+
+        static string[][] transpose(string[][] grid)
+        {
+            var result = new string[grid.Length][];
+            for (var i = 0; i < grid[0].Length; i++)
+            {
+                result[i] = new string[grid.Length];
+                for (var j = grid.Length - 1; j > -1; j--)
+                {
+                    result[i][j] = grid[j][i];
+                }
+            }
+            return result;
+        }
+
+        static string[][] pieceTogether(List<string[][]> grids)
+        {
+            int divisor = (grids.Count % 2 == 0) ? 2 : 3;
+            int combinedgridDimension = (int)Math.Sqrt(grids.Count()) * grids[0].Length;
+            string[][] newGrid = new string[combinedgridDimension][].Select(x => new string[combinedgridDimension]).ToArray();
+            var curr = 0;
+
+            for (int yDim = 0; yDim < combinedgridDimension; yDim += grids[0].Length)
+            {
+                for (int xDim = 0; xDim < combinedgridDimension; xDim += grids[0].Length)
+                {
+                    
+                    string[][] currGrid = grids[curr];
+
+                    for (int y = 0; y < currGrid.Length; y++)
+                    {
+                        for (int x = 0; x < currGrid.Length; x++)
+                        {
+                            newGrid[yDim + y][xDim + x] = currGrid[y][x];
+                        }
+                    }
+
+                    curr++;
+                }
+            }
+
+            return newGrid;
+        }
+
+        static void printGrid(string[][] grid)
+        {
+            for(int y = 0; y < grid.Length; y++)
+            {
+                for (int x = 0; x < grid.Length; x++)
+                {
+                    Console.Write(grid[y][x]);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+        }
+
+        static void Main(string[] args)
+        {
+            string[][] grid = {
+                new []{ ".", "#", "." },
+                new []{ ".", ".", "#" },
+                new []{ "#", "#", "#" },
+            };
+
+            Dictionary<string[][], string[][]> patternRules = new Dictionary<string[][], string[][]>(new GridComparer());
+
+            string[] lines = File.ReadAllLines("../../input.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Match match = Regex.Match(lines[i], @"(.+) => (.+)");
+                string[][] input = Convert1Dto2D(match.Groups[1].Value);
+                string[][] output = Convert1Dto2D(match.Groups[2].Value);
+
+                patternRules.Add(input, output);
+            }
+
+            for (int i = 0; i < 18; i++)
+            {
+                List<string[][]> splitGrids = splitGrid(grid);
+                for (int j = 0; j < splitGrids.Count; j++)
+                {
+                    splitGrids[j] = findMatch(splitGrids[j], patternRules);
+                }
+
+                grid = pieceTogether(splitGrids);
+
+                if(i == 4 || i == 17)
+                {
+                    int pixelsOn = 0;
+                    for (int j = 0; j < grid.Length; j++)
+                    {
+                        pixelsOn += grid[j].Where(x => x == "#").Count();
+                    }
+
+                    Console.WriteLine("There are " + pixelsOn + " pixels on after " + (i + 1) + " iterations");
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
